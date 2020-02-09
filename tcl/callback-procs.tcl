7b63b89c7c5fc4a2a2110d1be26bda79d2a19f14
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2020-02-09 15:34:41 +0000

    added editor hints and white-space cleanup

--- tcl/callback-procs.tcl
+++ tcl/callback-procs.tcl
@@ -14,69 +14,69 @@ namespace eval ::cookieconsent {
     # Provide hooks for installing/uninstalling the package
     #
     ad_proc -private after-install {} {
-	#
-	# Add additional parameters to acs-subsite
-	#
-	foreach {name description default datatype} {
-	    "Enabled"
-	    "Enable/Disable Cookie Consent for this Subsite"
-	    "0" "number"
+        #
+        # Add additional parameters to acs-subsite
+        #
+        foreach {name description default datatype} {
+            "Enabled"
+            "Enable/Disable Cookie Consent for this Subsite"
+            "0" "number"
 
-	    "Layout"
-	    "Layout of the Cookie Consent Widget; possible values: block|classic|edgeless|wire"
-	    "block" "string"
+            "Layout"
+            "Layout of the Cookie Consent Widget; possible values: block|classic|edgeless|wire"
+            "block" "string"
 
-	    "LearnMoreLink"
-	    "Link for learning more about Cookies"
-	    "https://cookiesandyou.com/" "string"
+            "LearnMoreLink"
+            "Link for learning more about Cookies"
+            "https://cookiesandyou.com/" "string"
 
-	    "Palette"
-	    "Color palette for the Cookie Consent Widget; possible values: default|oacs|honeybee|mono|neon|corporate"
-	    "default" "string"
+            "Palette"
+            "Color palette for the Cookie Consent Widget; possible values: default|oacs|honeybee|mono|neon|corporate"
+            "default" "string"
 
-	    "Position"
-	    "Position of the Cookie Consent Widget; possible values: bottom|top|pushdown|left|right"
-	    "pushdown" "string"
+            "Position"
+            "Position of the Cookie Consent Widget; possible values: bottom|top|pushdown|left|right"
+            "pushdown" "string"
 
-	    "DefaultPalette"
-	    "Default style: use the following settings, when CookieConsentPalette is set to 'default'"
-	    "popup {text #fff background #004570} button {text #000 background #f1d600}" "string"
+            "DefaultPalette"
+            "Default style: use the following settings, when CookieConsentPalette is set to 'default'"
+            "popup {text #fff background #004570} button {text #000 background #f1d600}" "string"
 
-	    "ExpiryDays"
-	    "Lifetime of the cookie"
-	    "365" "number"
-	} {
-	    apm_parameter_register "CookieConsent$name" \
-		$description "acs-subsite" $default $datatype "Cookie Consent"
-	}
+            "ExpiryDays"
+            "Lifetime of the cookie"
+            "365" "number"
+        } {
+            apm_parameter_register "CookieConsent$name" \
+                $description "acs-subsite" $default $datatype "Cookie Consent"
+        }
     }
 
     ad_proc -private before-uninstall {} {
-	#
-	# Remove the package specific parameters from acs-subsite
-	#
-	foreach parameter {
-	    Enabled
-	    Layout
-	    LearnMoreLink
-	    Palette
-	    Position
-	    DefaultPalette
-	    ExpiryDays
-	} {
-	    ns_log notice [list apm_parameter_unregister \
-			       -parameter "CookieConsent$parameter" \
-			       -package_key "acs-subsite" \
-			       "" ]
-	    ::try {
-		apm_parameter_unregister \
-		    -parameter "CookieConsent$parameter" \
-		    -package_key "acs-subsite" \
-		    ""
-	    } on error {errMsg} {
-		ns_log notice "apm_parameter_unregister of parameter CookieConsent$parameter lead to: $errMsg"
-	    }
-	}
+        #
+        # Remove the package specific parameters from acs-subsite
+        #
+        foreach parameter {
+            Enabled
+            Layout
+            LearnMoreLink
+            Palette
+            Position
+            DefaultPalette
+            ExpiryDays
+        } {
+            ns_log notice [list apm_parameter_unregister \
+                               -parameter "CookieConsent$parameter" \
+                               -package_key "acs-subsite" \
+                               "" ]
+            ::try {
+                apm_parameter_unregister \
+                    -parameter "CookieConsent$parameter" \
+                    -package_key "acs-subsite" \
+                    ""
+            } on error {errMsg} {
+                ns_log notice "apm_parameter_unregister of parameter CookieConsent$parameter lead to: $errMsg"
+            }
+        }
     }
 
 
@@ -88,9 +88,15 @@ namespace eval ::cookieconsent {
     #
     ad_proc -public -callback subsite::page_plugin -impl cookie-consent {
     } {
-	Implementation of subsite::page_plugin for cookie-consent
+        Implementation of subsite::page_plugin for cookie-consent
     } {
-	::cookieconsent::initialize_widget
+        ::cookieconsent::initialize_widget
     }
 
 }
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
